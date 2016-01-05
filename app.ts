a27aff7c87e0a9051827e71903d8c08d34887049
mbx2015 <mounirboussetta2013@gmail.com>
2016-01-05 12:12:41 +0000

    fix

--- app.ts
+++
@@ -1,6 +0,0 @@
-export interface Patient {
-  firstname: string;
-  lastname: string;
-  age: number;
-  phone: string;
-}
--- boot.ts
+++
@@ -1,8 +0,0 @@
-// this file is added jsut for testing git merge and push
-export class Person {
-  public name: string;
-  constructor(public name: string){this.name = name;}
-  greet(){
-    return "Hello, "+this.name;
-  }
-}
