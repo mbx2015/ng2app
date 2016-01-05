a71e0a41bec45684f4ea9dcbeb593e1b612ad746
mbx2015 <mounirboussetta2013@gmail.com>
2016-01-05 12:04:30 +0000

    modifying structure

---
+++ demo/app.ts
@@ -0,0 +1,6 @@
+export interface Patient {
+  firstname: string;
+  lastname: string;
+  age: number;
+  phone: string;
+}
---
+++ demo/boot.ts
@@ -0,0 +1,8 @@
+// this file is added jsut for testing git merge and push
+export class Person {
+  public name: string;
+  constructor(public name: string){this.name = name;}
+  greet(){
+    return "Hello, "+this.name;
+  }
+}
